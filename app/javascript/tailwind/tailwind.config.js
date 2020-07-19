d0d2d4bc97a6206645cc262c3fffb0a1cce2ad96
Garrett Dimon <garrett@adaptable.org>
2020-07-19 14:08:19 -0600

    Some additional Tailwind configuration and exploration.

--- app/javascript/tailwind/tailwind.config.js
+++ app/javascript/tailwind/tailwind.config.js
@@ -1,6 +1,6 @@
 module.exports = {
   purge: {
-    enabled: true,
+    enabled: false,
     content: [
       './app/**/*.html',
       './app/**/*.html.erb',
@@ -8,8 +8,26 @@ module.exports = {
     ],
   },
   theme: {
-    extend: {},
+    fontFamily: {
+      display: ['Helvetica', 'Arial', 'sans-serif'],
+      body: ['Georgia', 'Times', 'serif'],
+    },
+    extend: {
+      colors: {
+        'adpt-navy': '#111c4c',
+        'adpt-turquoise': '#32cc97',
+        'adpt-dark-turquoise': '#228f69',
+        'adpt-green': '#aacf01',
+        'adpt-dark-green': '#779100',
+        'adpt-pink': '#ff3571',
+        'adpt-dark-pink': '#b3254f',
+        'adpt-purple': '#dc38b1',
+        'adpt-dark-purple': '#9a287c',
+        'adpt-cornflower': '#677de6',
+        'adpt-dark-cornflower': '#4858a0',
+      },
+    },
   },
   variants: {},
-  plugins: [],
+  plugins: [require('@tailwindcss/ui')],
 }
