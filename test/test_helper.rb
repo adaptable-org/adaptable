583f2b960d12adab9d8ee8f1b30549ed224dc785
Garrett Dimon <garrett@adaptable.org>
2020-08-30 13:07:38 -0600

    Turning on the Focus minitest reporter.

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -6,7 +6,13 @@ ENV['RAILS_ENV'] ||= 'test'
 require_relative '../config/environment'
 require 'rails/test_help'
 
+require 'minitest/reporters'
+
 class ActiveSupport::TestCase
+  # Let's use the custom "Focus" minitest reporters...
+  # Minitest::Reporters.use! Minitest::Reporters::DefaultReporter.new
+  Minitest::Reporters.use! Minitest::Reporters::FocusReporter.new
+
   # Setup all fixtures in test/fixtures/*.yml for all tests in alphabetical order.
   fixtures :all
 
