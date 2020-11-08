50da4a1ba7958cce5075fd111eadb27e9ceb0782
Garrett Dimon <garrett@adaptable.org>
2020-11-08 14:33:46 -0700

    Updated Schema

--- db/schema.rb
+++ db/schema.rb
@@ -10,19 +10,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_11_08_180141) do
+ActiveRecord::Schema.define(version: 2020_11_08_201709) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "disabilities", force: :cascade do |t|
-    t.string "key"
+    t.string "key", null: false
     t.string "name"
     t.string "description"
     t.integer "parent_id"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
-    t.index ["key"], name: "index_disabilities_on_key"
+    t.index ["key"], name: "index_disabilities_on_key", unique: true
     t.index ["name"], name: "index_disabilities_on_name"
     t.index ["parent_id"], name: "index_disabilities_on_parent_id"
   end
@@ -40,8 +40,8 @@ ActiveRecord::Schema.define(version: 2020_11_08_180141) do
     t.string "url"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
-    t.string "key"
-    t.index ["key"], name: "index_organizations_on_key"
+    t.string "key", null: false
+    t.index ["key"], name: "index_organizations_on_key", unique: true
   end
 
 end
